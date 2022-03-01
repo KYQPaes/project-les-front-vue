5a30586c9a01c7e497bce69f91514aec8076e859
KYQPaes <kyq.paes@gmail.com>
2022-03-01 19:13:29 -0300

    KRP - Alterações Menu, adição de Icons

---
+++ src/components/Menu.vue
@@ -0,0 +1,37 @@
+<template>
+        <v-div>
+            <v-layout>
+                <v-flex xs13>
+                    <div class="text-xs-center">
+                        <v-img class="mx-auto" src="../images/logo.png" width="250" height="250"></v-img> 
+                    </div>
+                </v-flex>
+            </v-layout>
+            <v-toolbar>
+            </v-toolbar>
+                    <div>
+                        <!-- <a class="otherPage" href="http://localhost:8080/deus-1.0-SNAPSHOT/cupons.jsp" draggable="false">   -->
+
+                        <img src="images/icon/cupom.png" width="40" height="40" draggable="false" ></img> 
+                        <!-- </a> -->
+                        <!-- <a class="otherPage" href="http://localhost:8080/deus-1.0-SNAPSHOT/login.xhtml" draggable="false">   -->
+                            <img src="images/icon/login.png" id="user" width="40" height="40" draggable="false" ></img> 
+                        <!-- </a> -->
+                        <!-- <a class="otherPage" href="http://localhost:8080/deus-1.0-SNAPSHOT/carrinho.jsp" draggable="false">   -->
+                        <v-icon width="40" height="40">
+                            mdi-percent
+                        </v-icon>
+                            <img src="images/icon/cart.png" width="40" height="40" draggable="false" ></img> 
+                        <!-- </a> -->
+                    </div>
+        </v-div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <Menu />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import Menu from '../components/Menu.vue'
 export default {
   name: 'HomeView',
   components: {
+    Menu
   }
 }
 </script>
