300e8d9a6c512e2597f6abb998b9aa4d050a4d0b
KYQPaes <kyq.paes@gmail.com>
2022-03-01 22:09:53 -0300

    KRP - Modelo Exemplo de Menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,37 +1,84 @@
 <template>
         <v-div>
             <v-layout>
-                <v-flex xs13>
+                <v-flex xs12>
                     <div class="text-xs-center">
                         <v-img class="mx-auto" src="../images/logo.png" width="250" height="250"></v-img> 
                     </div>
                 </v-flex>
             </v-layout>
-            <v-toolbar>
-            </v-toolbar>
-                    <div>
+            <v-layout style="margin-bottom:10px">
+                <v-spacer>
+                </v-spacer>
+                <v-flex xs1>
                         <!-- <a class="otherPage" href="http://localhost:8080/deus-1.0-SNAPSHOT/cupons.jsp" draggable="false">   -->
-
-                        <img src="images/icon/cupom.png" width="40" height="40" draggable="false" ></img> 
+                        <v-btn id="button" fab small text>
+                            <v-icon :size="size" color="black">
+                                mdi-percent
+                            </v-icon>
+                        </v-btn>
                         <!-- </a> -->
                         <!-- <a class="otherPage" href="http://localhost:8080/deus-1.0-SNAPSHOT/login.xhtml" draggable="false">   -->
-                            <img src="images/icon/login.png" id="user" width="40" height="40" draggable="false" ></img> 
+                        <v-btn id="button" fab small text>
+                            <v-icon :size="size" color="black">
+                                account_circle
+                            </v-icon>
+                        </v-btn>
                         <!-- </a> -->
                         <!-- <a class="otherPage" href="http://localhost:8080/deus-1.0-SNAPSHOT/carrinho.jsp" draggable="false">   -->
-                        <v-icon width="40" height="40">
-                            mdi-percent
-                        </v-icon>
-                            <img src="images/icon/cart.png" width="40" height="40" draggable="false" ></img> 
+                        <v-btn id="button" fab small text>
+                            <v-icon :size="size" color="black">
+                                shopping_cart
+                            </v-icon>
+                        </v-btn>
                         <!-- </a> -->
-                    </div>
+                </v-flex>
+            </v-layout>
+            <v-toolbar>
+                <v-layout>
+                    <v-flex xs4 style="border:1px solid black">
+                        <v-row >
+                            <v-col>
+                                Masculino
+                            </v-col>
+                            <v-col>
+                                Feminino
+                            </v-col>
+                            <v-col>
+                                Personalizadas
+                            </v-col>
+                            <v-col>
+                                Carbono
+                            </v-col>
+                            <v-col>
+                                Documento
+                            </v-col>
+                        </v-row>
+                    </v-flex>
+                    <v-spacer></v-spacer>
+                    <v-flex xs2>
+                        <v-text-field outlined hide-details label="Pesquise Aqui..." ></v-text-field>
+                    </v-flex>
+                </v-layout>
+            </v-toolbar>
         </v-div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            size: 30,
+        }
+    }
 }
 </script>
 <style scoped>
-
+.v-input__control{
+  max-height: 30px !important;
+}
+.vbutton{
+    margin-right:10px;
+    margin-bottom: 5px
+}
 </style>
