cd8c0fdb97d12508763cca35eab19d2853b67588
LeonardoNarimatsu <leonardo-eiji@live.com>
2022-03-02 17:09:23 -0300

    background

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,44 @@
 <template>
-  <div class="home">
+  <div>
     <Menu />
+    <div display: flex; align-content: center, align-items: center  align-self: center; flex-direction: column;>
+      
+    <v-card :style="{backgroundImage:'url(https://wallpaperscute.com/wp-content/uploads/2018/07/Graffiti-Wall-Desktop-Backgrounds-HD.jpg)',backgroundSize:'cover'
+    } ">
+    <div style="background-color: white ;width: 50%">
 
-  <v-layout class="text-xs-center">
-    <v-row justify="center" style="margin:40px">
-    <h1>
-      Carteiras Personalizadas
-    </h1>
-    </v-row>
-  </v-layout >
+    
 
-  <v-layout >
-    <v-row justify="center" style="margin:40px">
-    <v-flex xs6>
-      <v-carousel>
-        <v-carousel-item
-          v-for="(item,i) in items"
-          :key="i"
-          :src="item.src"
-          reverse-transition="fade-transition"
-          transition="fade-transition"
-        ></v-carousel-item>
-      </v-carousel>
-    </v-flex>
-  </v-row>
-  </v-layout>
 
+    <v-layout class="text-xs-center" >
+        <v-row justify="center" style="margin:40px">
+        <h1>
+          Carteiras Personalizadas
+        </h1>
+        </v-row>
+      </v-layout >
+
+      <v-layout >
+        <v-row justify="center" style="margin:40px">
+          <v-flex xs6>
+            <v-carousel>
+              <v-carousel-item
+                v-for="(item,i) in items"
+                :key="i"
+                :src="item.src"
+                reverse-transition="fade-transition"
+                transition="fade-transition"
+              ></v-carousel-item>
+            </v-carousel>
+          </v-flex>
+        </v-row>
+      </v-layout>
   </div>
+  </v-card>
+  </div>
+  </div>
+
+ 
   
 </template>
 
