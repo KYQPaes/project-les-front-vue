74d3be38f72f30b94a348da3abb67bf88d56ce75
LeonardoNarimatsu <leonardo-eiji@live.com>
2022-03-01 23:01:32 -0300

    Home Carroseu

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="home">
     <Menu />
+
+  <v-layout class="text-xs-center">
+    <v-row justify="center" style="margin:40px">
+    <h1>
+      Carteiras Personalizadas
+    </h1>
+    </v-row>
+  </v-layout >
+
+  <v-layout >
+    <v-row justify="center" style="margin:40px">
+    <v-flex xs6>
+      <v-carousel>
+        <v-carousel-item
+          v-for="(item,i) in items"
+          :key="i"
+          :src="item.src"
+          reverse-transition="fade-transition"
+          transition="fade-transition"
+        ></v-carousel-item>
+      </v-carousel>
+    </v-flex>
+  </v-row>
+  </v-layout>
+
   </div>
+  
 </template>
 
 <script>
@@ -10,6 +36,26 @@ export default {
   name: 'HomeView',
   components: {
     Menu
+  },
+
+  data () {
+      return {
+        items: [
+          {
+            src:  'https://supercarteira.com.br/img/wallets/maro01/carteira-de-papel-tyvek-mar-oriental-frente.jpg',
+          },
+          {
+            src: 'https://supercarteira.com.br/img/wallets/astr01/carteira-de-papel-tyvek-astronauta-frente.jpg',
+          },
+          {
+            src: 'https://supercarteira.com.br/img/wallets/univ01/carteira-de-papel-tyvek-universo-frente.jpg',
+          },
+          {
+            src: 'https://supercarteira.com.br/img/wallets/juli01/carteira-de-papel-tyvek-julius-frente.jpg',
+          },
+        ],
+      }
+    },
   }
-}
+
 </script>
