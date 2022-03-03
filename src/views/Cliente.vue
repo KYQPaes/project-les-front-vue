94ec5df491abbc619f040d3eb7d263602d6e5c83
KYQPaes <kyq.paes@gmail.com>
2022-03-02 21:46:15 -0300

    KRP - Criação de Cliente

---
+++ src/views/Cliente.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <Menu />
+        <v-container>
+            <v-card elevation="10" style="height:500px">
+            </v-card>
+        </v-container>
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Menu from '../components/Menu.vue'
+import Footer from '../components/Footer.vue'
+export default {
+    name: 'Cliente',
+    components: {
+        Menu,
+        Footer
+    }
+}
+</script>
+
+<style>
+
+</style>
