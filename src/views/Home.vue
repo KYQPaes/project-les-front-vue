35d6378f93ad982a34d8bd834a202a01e18e4a49
KYQPaes <kyq.paes@gmail.com>
2022-03-02 21:46:04 -0300

    KRP - Uso do footer na home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
     <Menu />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Menu from '../components/Menu.vue'
+import Footer from '../components/Footer.vue'
 export default {
   name: 'HomeView',
   components: {
-    Menu
+    Menu,
+    Footer
   }
 }
 </script>
