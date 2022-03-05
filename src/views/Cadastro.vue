7c701222e211c61939d61a110a0c7fd2436d9c22
LeonardoNarimatsu <leonardo-eiji@live.com>
2022-03-05 20:08:24 -0300

    date

--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -12,61 +12,56 @@
               <v-card-text>
                 <v-layout>
                   <v-flex xs10>
-                    <v-text-field v-model="nome" :rules="emptyRules" type="NomeCompleto" label="Nome Completo" placeholder="NomeCompleto" />
+                    <v-text-field v-model="cliente.nome" :rules="emptyRules" type="NomeCompleto" label="Nome Completo" placeholder="NomeCompleto" />
                   </v-flex>
                 </v-layout>
 
                 <v-layout>
                   <v-flex xs6>
-                    <v-select :items="items" label="Gênero" v-model="genero" :rules="emptyRules"></v-select>
+                    <v-select :items="items" label="Gênero" v-model="cliente.genero" :rules="emptyRules"></v-select>
                   </v-flex>
 
                   <v-flex xs6>
-                    <v-menu ref="menu1" v-model="menu1" :close-on-content-click="false" transition="scale-transition" offset-y max-width="290px" min-width="auto">
-                      <template v-slot:activator="{ on, attrs }">
-                        <v-text-field v-model="dateFormatted" :rules="emptyRules" label="Data de Nascimento" persistent-hint prepend-icon="mdi-calendar" v-bind="attrs" @blur="date = parseDate(dateFormatted)" v-on="on"></v-text-field>
-                      </template>
-                      <v-date-picker v-model="date" no-title @input="menu1 = false"></v-date-picker>
-                    </v-menu>
+                    <v-text-field type="date" v-model="cliente.data_nasc" :rules="emptyRules" label="Data de Nascimento"></v-text-field>
                   </v-flex>
                 </v-layout>
 
                 <v-layout>
                   <v-flex xs4>
-                    <v-text-field v-mask="'###.###.###.##'" type="CPF" label="CPF" placeholder="CPF" v-model="CPF" :rules="emptyRules" />
+                    <v-text-field v-mask="'###.###.###.##'" type="CPF" label="CPF" placeholder="CPF" v-model="cliente.cpf" :rules="emptyRules" />
                   </v-flex>
                   <v-flex xs8>
-                    <v-text-field v-model="End" :rules="emptyRules" type="Endereço Residencial" label="Endereço Residencial" placeholder="Endereço Residencial" />
+                    <v-text-field v-model="cliente.endereco" :rules="emptyRules" type="Endereço Residencial" label="Endereço Residencial" placeholder="Endereço Residencial" />
                   </v-flex>
                 </v-layout>
 
                 <v-layout>
                   <v-flex xs4>
-                    <v-select :items="TipoTelefone" label="Tipo de Telefone" v-model="TipoDeTelefone"></v-select>
+                    <v-select :items="TipoTelefone" label="Tipo de Telefone" v-model="cliente.tptelefone"></v-select>
                   </v-flex>
                   <v-flex xs2>
-                    <v-text-field v-mask="'##'" v-model="DDD" :rules="emptyRules" type="DDD" label="DDD" placeholder="DDD" />
+                    <v-text-field v-mask="'##'" v-model="cliente.ddd" :rules="emptyRules" type="DDD" label="DDD" placeholder="DDD" />
                   </v-flex>
                   <v-flex xs5>
-                    <v-text-field v-mask="TipoDeTelefone == 'Residencial' ? '####-####' : '9####-####'" v-model="Telefone" :rules="emptyRules" type="Numero de Telefone" label="Numero de Telefone" placeholder="Numero de Telefone" />
+                    <v-text-field v-mask="cliente.tptelefone == 'Residencial' ? '####-####' : '9####-####'" v-model="cliente.telefone" :rules="emptyRules" type="Numero de Telefone" label="Numero de Telefone" placeholder="Numero de Telefone" />
                   </v-flex>
                 </v-layout>
 
                 <v-layout>
                   <v-flex xs6>
-                    <v-text-field v-model="email" :rules="emptyRules" type="email" label="Email" placeholder="Email" />
+                    <v-text-field v-model="cliente.email" :rules="emptyRules" type="email" label="Email" placeholder="Email" />
                   </v-flex>
                   <v-flex xs6>
-                    <v-text-field v-model="ConfirmarEmail" :rules="[this.email === this.ConfirmarEmail || 'campos diferentes', (v) => !!v || 'Campo Obrigatório']" type="Confirmar Email" label="Confirmar Email" placeholder="Confirmar Email" />
+                    <v-text-field v-model="ConfirmarEmail" :rules="[this.cliente.email === this.ConfirmarEmail || 'campos diferentes', (v) => !!v || 'Campo Obrigatório']" type="Confirmar Email" label="Confirmar Email" placeholder="Confirmar Email" />
                   </v-flex>
                 </v-layout>
 
                 <v-layout>
                   <v-flex xs6>
-                    <v-text-field validate-on-blur v-model="password" :rules="[rules.password]" class="pass" :type="passwordShow ? 'text' : 'password'" label="Senha" placeholder="Senha" :append-icon="passwordShow ? 'mdi-eye' : 'mdi-eye-off'" @click:append="passwordShow = !passwordShow" />
+                    <v-text-field validate-on-blur v-model="cliente.senha" :rules="[rules.password]" class="pass" :type="passwordShow ? 'text' : 'password'" label="Senha" placeholder="Senha" :append-icon="passwordShow ? 'mdi-eye' : 'mdi-eye-off'" @click:append="passwordShow = !passwordShow" />
                   </v-flex>
                   <v-flex xs6>
-                    <v-text-field v-model="ConfirmarPassword" :rules="[this.password === this.ConfirmarPassword || 'campos diferentes', (v) => !!v || 'Campo Obrigatório']" :type="passwordShow2 ? 'text' : 'password'" label="Confirmar Senha" placeholder="Senha" :append-icon="passwordShow2 ? 'mdi-eye' : 'mdi-eye-off'" @click:append="passwordShow2 = !passwordShow2" />
+                    <v-text-field v-model="ConfirmarPassword" :rules="[this.cliente.senha === this.ConfirmarPassword || 'campos diferentes', (v) => !!v || 'Campo Obrigatório']" :type="passwordShow2 ? 'text' : 'password'" label="Confirmar Senha" placeholder="Senha" :append-icon="passwordShow2 ? 'mdi-eye' : 'mdi-eye-off'" @click:append="passwordShow2 = !passwordShow2" />
                   </v-flex>
                 </v-layout>
               </v-card-text>
@@ -96,12 +91,16 @@
           </v-card>
         </v-col>
       </v-main>
-      <v-snackbar top color="green" v-model="snackbar"> Cadastro Efetuado com sucesso </v-snackbar>
+      <v-snackbar top :color="error == true ? 'error' : 'green'" v-model="snackbar">
+        <span v-if="error == false"> Cadastro efetuado com sucesso </span>
+        <span v-else> Erro ao realizar o cadastro, verifique todos os campos </span>
+      </v-snackbar>
     </v-card>
   </v-app>
 </template>
 
 <script>
+import clienteService from "@/service/clientes";
 export default {
   data: (vm) => ({
     passwordShow: false,
@@ -111,28 +110,27 @@ export default {
     menu1: false,
     menu2: false,
 
+    error: false,
+
     items: ["Masculino", "Feminino"],
     TipoTelefone: ["Residencial", "Móvel"],
 
-    date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000).toISOString().substr(0, 10),
-    dateFormatted: vm.formatDate(new Date(Date.now() - new Date().getTimezoneOffset() * 60000).toISOString().substr(0, 10)),
-    CPF: "",
-    DDD: "",
-    TipoDeTelefone: "Residencial",
-    Telefone: "",
-    dateFormatted: "",
-    email: "",
-    password: "",
     ConfirmarEmail: "",
     ConfirmarPassword: "",
-    DDD: "",
-    Telefone: "",
-    End: "",
-    CPF: "",
-    dateFormatted: "",
-    nome: "",
-    genero: "",
     checkbox: "",
+
+    cliente: {
+      nome: "",
+      genero: "",
+      data_nasc: "",
+      cpf: "",
+      endereco: "",
+      tptelefone: "Residencial",
+      ddd: "",
+      telefone: "",
+      senha: "",
+      email: "",
+    },
 
     emptyRules: [(v) => !!v || "Campo Obrigatório"],
 
@@ -145,40 +143,25 @@ export default {
     },
   }),
 
-  computed: {
-    computedDateFormatted() {
-      return this.formatDate(this.date);
-    },
-  },
-
-  watch: {
-    date(val) {
-      this.dateFormatted = this.formatDate(this.date);
-    },
-  },
-
   methods: {
-    formatDate(date) {
-      if (!date) return null;
-
-      const [year, month, day] = date.split("-");
-      return `${month}/${day}/${year}`;
-    },
-    parseDate(date) {
-      if (!date) return null;
-
-      const [month, day, year] = date.split("/");
-      return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
-    },
-
     submitHandler() {
+      this.error = false;
       if (this.$refs.form.validate()) {
         this.loading = true;
-        setTimeout(() => {
-          this.loading = false;
-          this.snackbar = true;
-          this.$router.push({ path: "/login" });
-        }, 2000);
+        this.loading = false;
+        clienteService.create(this.cliente).then((response) => {
+          if (response.data.length > 0) {
+            this.error = false;
+            this.snackbar = true;
+            setTimeout(() => {
+              localStorage.setItem("cliente", JSON.stringify(response.data));
+              this.$router.push({ path: "/" });
+            }, 1500);
+          } else {
+            this.error = true;
+            this.snackbar = true;
+          }
+        });
       }
     },
   },
