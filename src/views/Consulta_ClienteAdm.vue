af33ccfd4fa513426c21c2de99a6baf975248610
KYQPaes <kyq.paes@gmail.com>
2022-04-02 20:25:20 -0300

    KRP - Funcionando com o back e input para pesquisar

--- src/views/Consulta_ClienteAdm.vue
+++ src/views/Consulta_ClienteAdm.vue
@@ -5,16 +5,27 @@
       <v-layout>
         <v-flex xs8 style="margin-left: 17%">
           <v-card elevation="10" class="justify-center">
-            <v-data-table :headers="headers" :items="desserts" sort-by="calories" class="elevation-1">
+            <v-data-table :search="search" :headers="headers" :items="clientes" sort-by="calories" class="elevation-1">
               <template v-slot:top>
                 <v-toolbar flat class="d-flex justify-center">
                   <v-toolbar-title>Contas Cadastradas</v-toolbar-title>
-                  <v-divider class="mx-4" inset></v-divider>
-                  <v-spacer></v-spacer>
                 </v-toolbar>
+                <v-layout class="justify-center">
+                  <v-flex xs3>
+                    <v-text-field v-model="search" label="Pesquisar..."></v-text-field>
+                  </v-flex>
+                </v-layout>
+              </template>
+              <template v-slot:item.inativar="{ item }">
+                <span v-if="item.inativar">
+                  Inativado
+                </span>
+                <span v-else>
+                  Ativado
+                </span>
               </template>
               <template v-slot:item.actions="{ item }">
-                <v-icon small class="mr-2" @click="editItem(item)"> description </v-icon>
+                <v-icon small class="mr-2" @click="$router.push({ path: `/consultaContaAdm/${item.id}` })"> description </v-icon>
               </template>
               <template v-slot:no-data>
                 <v-btn color="primary" @click="initialize"> Reset </v-btn>
@@ -40,33 +51,19 @@ export default {
   name: "Cliente",
   data: (vm) => ({
     headers: [
-      { text: "ID da conta", value: "ID" },
-      {
-        text: "Email",
-        align: "start",
-        filterable: false,
-        value: "email",
-      },
-      {
-        text: "Nome",
-        align: "start",
-        filterable: false,
-        value: "nome",
-      },
+      { text: "ID da conta", value: "id" },
+      {text: "Email",align: "start",value: "email"},
+      {text: "Nome",align: "start",value: "nome"},
       { text: "Telefone", value: "telefone" },
-      {
-        text: "Status da conta",
-        align: "start",
-        filterable: false,
-        value: "status",
-      },
-      ,
+      {text: "Status da conta",align: "start",value: "inativar"},
       { text: "Pedidos", value: "actions", sortable: false },
     ],
+    clientes: [],
+    search: '',
   }),
 
   created() {
-    this.initialize();
+    this.list();
   },
 
   components: {
@@ -76,7 +73,6 @@ export default {
   mounted() {
     this.cliente = JSON.parse(localStorage.getItem("cliente"));
   },
-  computed: {},
 
   watch: {
     dialog(val) {
@@ -88,51 +84,10 @@ export default {
   },
 
   methods: {
-    initialize() {
-      this.desserts = [
-        {
-          ID: 1,
-          email: "leonardo-eiji@live.com",
-          nome: "leonardo",
-          telefone: "97474-5487",
-          status: "Ativo",
-        },
-        {
-          ID: 2,
-          email: "roverto-eiji@live.com",
-          nome: "aluis",
-          telefone: "97474-5487",
-          status: "Ativo",
-        },
-        {
-          ID: 7,
-          email: "bruna-eiji@live.com",
-          nome: "bruna",
-          telefone: "97474-5487",
-          status: "Ativo",
-        },
-        {
-          ID: 9,
-          email: "kayque-eiji@live.com",
-          nome: "kayque",
-          telefone: "97474-5487",
-          status: "Inativo",
-        },
-        {
-          ID: 3,
-          email: "joão-eiji@live.com",
-          nome: "joão",
-          telefone: "97474-5487",
-          status: "Inativo",
-        },
-        {
-          ID: 5,
-          email: "valtencir-eiji@live.com",
-          nome: "val",
-          telefone: "97474-5487",
-          status: "Ativo",
-        },
-      ];
+    list() {
+      clienteService.list().then((response) => {
+        this.clientes = response.data;
+      });
     },
   },
 };
